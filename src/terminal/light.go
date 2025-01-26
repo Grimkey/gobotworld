593dcbac3df7ceefbb708abdc464cd0dda91e9f6
James White <grimkey@gmail.com>
2025-01-26 15:35:14 -0600

    feat: Fixing East facing vision.
    The problem was that east was not visible because Atan was calculating out the value pixels.
    
    Because West worked really well. Just mirrored the West function.
    
    Also removed a bunch of unused constants.

--- src/terminal/light.go
+++ src/terminal/light.go
@@ -8,31 +8,6 @@ import (
 	"math"
 )
 
-var (
-	sqrt3over2 = math.Sqrt(3) / 2
-	sqrt2over2 = math.Sqrt(2) / 2
-
-	arcTan30 = math.Atan2(sqrt3over2, 0.5)
-	arcTan45 = math.Atan2(sqrt2over2, sqrt2over2)
-	arcTan60 = math.Atan2(0.5, sqrt3over2)
-	arcTan90 = math.Atan2(0, 1)
-
-	arcTan120 = math.Atan2(-0.5, sqrt3over2)
-	arcTan135 = math.Atan2(-sqrt2over2, sqrt2over2)
-	arcTan150 = math.Atan2(-sqrt3over2, 0.5)
-	arcTan180 = math.Atan2(-1, 0)
-
-	arcTan210 = math.Atan2(-sqrt3over2, -0.5)
-	arcTan225 = math.Atan2(-sqrt2over2, -sqrt2over2)
-	arcTan240 = math.Atan2(-0.5, -sqrt3over2)
-	arcTan270 = math.Atan2(0, -1)
-
-	arcTan300 = math.Atan2(1/2, -sqrt3over2)
-	arcTan315 = math.Atan2(sqrt2over2, -sqrt2over2)
-	arcTan330 = math.Atan2(sqrt3over2, -0.5)
-	arcTan360 = math.Atan2(1, 0)
-)
-
 func SenseValue(pt image.Point, wld world.World) float32 {
 	dist := geometry.Distance(*wld.Player.Location, pt)
 	if dist > 15 {
@@ -71,8 +46,8 @@ func WestVision(pt image.Point, char image.Point) bool {
 }
 
 func EastVision(pt image.Point, char image.Point) bool {
-	currentAngle := math.Atan2(float64(char.Y-pt.Y), float64(char.X-2-pt.X))
-	return currentAngle < math.Pi/8 && currentAngle > 15*math.Pi/8
+	currentAngle := math.Atan2(float64(char.Y-pt.Y), float64(pt.X-(char.X-2)))
+	return currentAngle > -math.Pi/8 && currentAngle < math.Pi/8
 }
 
 func LightValue(pt image.Point, viewable geometry.Window, world world.World) object.LightBlock {
